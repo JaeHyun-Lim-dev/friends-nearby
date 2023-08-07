85688a0b19fab450e2dc44741da2c9922771bb52
JaeHyun-Lim-FE <bonny06@naver.com>
2023-08-07 19:41:17 +0900

    feat: firebase 연동 테스트용 회원가입 코드 추가

--- src/pages/CreateAccount.vue
+++ src/pages/CreateAccount.vue
@@ -1,40 +1,64 @@
 <template>
   <div class="fullpage">
-    <form v-on:submit.prevent="submitForm">
-      <div>
-        <label for="id">id: </label
-        ><input v-model="inputId" placeholder="아이디를 입력해주세요." />
-      </div>
-      <div>
-        <label for="id">pw: </label
-        ><input v-model="inputPw1" placeholder="비밀번호를 입력해주세요." />
-      </div>
-      <div>
-        <label for="id">확인: </label
-        ><input
-          v-model="inputPw2"
-          placeholder="비밀번호를 한번 더 입력해주세요."
-        />
-      </div>
-      <button class="app-button" type="submit">Submit</button>
-    </form>
+    <div class="input-label">
+      <label for="id">id</label
+      ><input v-model="inputId" placeholder="아이디를 입력해주세요." />
+    </div>
+    <div class="input-label">
+      <label for="name">이름</label
+      ><input v-model="inputName" placeholder="이름을 입력해주세요." />
+    </div>
+    <div class="input-label">
+      <label for="id">비밀번호</label
+      ><input
+        type="password"
+        v-model="inputPw1"
+        placeholder="비밀번호를 입력해주세요."
+      />
+    </div>
+    <div class="input-label">
+      <label for="id">확인</label
+      ><input
+        type="password"
+        v-model="inputPw2"
+        placeholder="비밀번호를 한번 더 입력해주세요."
+      />
+    </div>
+    <button class="app-button" @click="submitForm">Submit</button>
   </div>
 </template>
 
 <script>
+import firebase from "firebase/compat/app";
 export default {
   name: "CreateAccountPage",
   props: {},
   data: function () {
     return {
       inputId: "",
+      inputName: "",
       inputPw1: "",
       inputPw2: "",
     };
   },
   methods: {
-    submitForm() {
-      alert(`submit: ${this.inputId}, ${this.inputPw1}, ${this.inputPw2}`);
+    async submitForm() {
+      if (this.inputPw1 != this.inputPw2) {
+        alert("비밀번호가 같지 않습니다. 다시 확인해주세요.");
+        return;
+      }
+
+      await firebase
+        .database()
+        .ref("users/" + this.inputId)
+        .set({
+          id: this.inputId,
+          name: this.inputName,
+          password: this.inputPw1,
+          point: 0,
+        });
+
+      this.$router.push("login");
     },
   },
   setup() {},
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="fullpage">
-    <div>
-      <label for="id">id: </label
+    <div class="input-label">
+      <label for="id">id</label
       ><input v-model="inputId" placeholder="아이디를 입력해주세요." />
     </div>
-    <div>
-      <label for="id">pw: </label
+    <div class="input-label">
+      <label for="id">pw</label
       ><input v-model="inputPw" placeholder="비밀번호를 입력해주세요." />
     </div>
-    <router-link class="app-button" to="check-bluetooth">로그인</router-link>
+    <div class="col-container">
+      <router-link class="app-button" to="check-bluetooth">로그인</router-link>
+      <router-link class="app-button" to="create-account">회원가입</router-link>
+    </div>
   </div>
 </template>
 
