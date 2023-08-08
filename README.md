000e619857cde73b6c8c8433e3cc86bef44e48bb
JaeHyun-Lim-FE <bonny06@naver.com>
2023-08-08 12:03:37 +0900

    chore: baseUrl 변경

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# friends-nearby
+# liiv-arround
 
 ## Project setup
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/friends-nearby",
+  publicPath: "/liiv-arround",
   transpileDependencies: true,
 });
